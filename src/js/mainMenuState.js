71392a6f19cafa07ac5e12c879f70615f0b0347f
Alice <alicenara165@gmail.com>
2015-01-25 07:44:37 +0100

    Added music and upgraded the room

--- src/js/mainMenuState.js
+++ src/js/mainMenuState.js
@@ -7,12 +7,21 @@ var MainMenuState = {
         this.load.spritesheet('button-start', 'assets/gui/btnew-game.png', 160, 32);
         this.load.image('bg', 'assets/sprites/background.png');
         this.load.image('logo', 'assets/gui/logo.png');
+        this.load.audio('metalClip', 'assets/sounds/metalClick.ogg');
+        this.load.audio('bgMusic', 'assets/sounds/bgMusic.mp3');
     },
 
     create: function() {
         tilesprite=this.add.tileSprite(0, 0, 800, 600, 'bg');
+        // Audio
+        fx = this.add.audio('metalClip');
+        fx.allowMultiple = true;
+        fx.addMarker('metalClip', 0, 1);
+        music = this.add.audio('bgMusic');
+        music.play();
+
         this.add.sprite(250, 100, 'logo');
-        this.add.button(320, 450, 'button-start', function(){changeState('Gameplay')}, this, 0, 1, 2);
+        this.add.button(320, 450, 'button-start', function(){ fx.play("metalClip"); changeState('Gameplay'); }, this, 0, 1, 2);
     },
 
     update: function() {
